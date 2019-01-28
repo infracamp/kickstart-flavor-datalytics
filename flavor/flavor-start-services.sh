0215688f155e40041667ffc82653cc329a6abef9
Matthias <m@tth.es>
2019-01-28 17:02:56 +0100

    lowered required minimum userid to 100

--- flavor/flavor-start-services.sh
+++ flavor/flavor-start-services.sh
@@ -10,5 +10,5 @@ sudo -E -s -u user jupyter notebook --ip=0.0.0.0 --port=4000 --NotebookApp.token
 
 echo "| RStudio on port 4200 (http://localhost:4200) User: user Pass: pass"
 
-/usr/lib/rstudio-server/bin/rserver --server-daemonize=0 &
+/usr/lib/rstudio-server/bin/rserver --server-daemonize=0  --auth-minimum-user-id=100 &
 
