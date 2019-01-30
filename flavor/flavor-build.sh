f6c3ee73326431cf29a6bf357ac87da970f1c1e3
Matthias <m@tth.es>
2019-01-30 16:52:22 +0100

    changed R USER_LIBS to /opt

--- flavor/flavor-build.sh
+++ flavor/flavor-build.sh
@@ -31,7 +31,7 @@ echo "www-port=4200"         >> /etc/rstudio/rserver.conf
 ## Make studio start in /opt instead of /home/user
 echo "session-default-working-dir=/opt"     >> /etc/rstudio/rsession.conf
 echo "session-default-new-project-dir=/opt" >> /etc/rstudio/rsession.conf
-
+echo "r-libs-user=/opt/R/x86_64-pc-linux-gnu-library/3.4" >> /etc/rstudio/rsession.conf
 ## Don't suspend any session
 echo "session-timeout-minutes=0" >> /etc/rstudio/rsession.conf
 
@@ -52,6 +52,10 @@ sudo R -e 'install.packages("jsonlite",     repos="http://cran.rstudio.com/")'
 sudo R -e 'install.packages("base64enc",    repos="http://cran.rstudio.com/")'
 sudo R -e 'install.packages("rprojroot",    repos="http://cran.rstudio.com/")'
 sudo R -e 'install.packages("rmarkdown",    repos="http://cran.rstudio.com/")'
+#
+# Required for shiny web apps
+#sudo R -e 'install.packages("shiny",    repos="http://cran.rstudio.com/")'
 
 
-
+# Set R User-Libs path to /opt
+echo "R_LIBS_USER='/opt/R/x86_64-pc-linux-gnu-library/3.4'" >> /etc/R/Renviron
